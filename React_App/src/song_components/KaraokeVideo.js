651a13375cd792c66ec4b872155946064e37fb8c
Kenneth L Powell <52848895+kailclaskan@users.noreply.github.com>
2021-08-17 16:09:50 -0700

    Finally got my files working properly. API key should be added and working as expected.

--- React_App/src/song_components/KaraokeVideo.js
+++ React_App/src/song_components/KaraokeVideo.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import {YT_API_KEY} from "../secret";
 
 //Need to make it so this is NOT so public.
 
@@ -7,7 +8,7 @@ let KaraokeVideo = ({songTitle, songArtist}) => {
     let [videoId, setVideoId] = useState("");
     useEffect(() => {
         let data = async () =>{
-            let res = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${songTitle} ${songArtist} karaoke&key=${/*Add api your api key*/}`)
+            let res = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${songTitle} ${songArtist} karaoke&key=${YT_API_KEY}`);
             for(let song of res.data.items){
                 if(song.snippet.channelId !== "UCbqcG1rdt9LMwOJN4PyGTKg" && song.snippet.channelId !== "UCwTRjvjVge51X-ILJ4i22ew"){
                     setVideoId(song.id.videoId);
